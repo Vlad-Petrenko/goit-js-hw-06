c573944476e5433e1279705f9322e5c0f5faf9a2
Vlad-Petrenko <92335560+Vlad-Petrenko@users.noreply.github.com>
2022-03-24 22:04:24 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const createIdForArray = document.querySelector("ul#ingredients");
+
+const list = ingredients.map((element) => {
+  const listEl = document.createElement("li");
+  listEl.textContent = element;
+  listEl.classList.add("item");
+  return listEl;
+});
+createIdForArray.prepend(...list);
