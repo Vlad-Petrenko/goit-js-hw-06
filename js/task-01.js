bdaaff674c44045a9efdfc66936843b2b79e7bbd
Vlad-Petrenko <92335560+Vlad-Petrenko@users.noreply.github.com>
2022-03-24 21:37:44 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categoriesNames = document.querySelector("#categories");
+const categoriesItems = categoriesNames.querySelectorAll("li.item");
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+const onElItem = categoriesItems.forEach((elementItem) => {
+  console.log(`Category: ${elementItem.firstElementChild.textContent}`);
+  console.log(`Elements: ${elementItem.querySelectorAll("li").length}`);
+});
