71e23bb822ee8752d192cd155981870bc9e69da8
Vlad-Petrenko <92335560+Vlad-Petrenko@users.noreply.github.com>
2022-03-26 01:37:53 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector("form.login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть заполнены!");
+  }
+
+  () => {
+    const email = event.currentTarget.elements["email"].value;
+    const passwordName = event.currentTarget.elements["password"].value;
+
+    return (formData = {
+      email,
+      passwordName,
+    });
+  };
+  console.log(formData);
+  //   console.log(`email: ${email.value}, Password: ${password.value}`);
+  //   event.currentTarget.reset();
+}
