cbc61577ee0811450f63966d71f709120f463eee
Vlad-Petrenko <92335560+Vlad-Petrenko@users.noreply.github.com>
2022-03-27 22:42:25 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputNumber = document.querySelector("#controls input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const divBoxes = document.querySelector("div#boxes");
+
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const array = [];
+
+  for (let i = 1; i <= Number(inputNumber.value); i += 1) {
+    array.push(
+      `<div style="background-color: ${getRandomHexColor()}; width: ${
+        20 + i * 10
+      }px; height: ${20 + i * 10}px"></div>`
+    );
+  }
+  divBoxes.insertAdjacentHTML("afterbegin", array.join(""));
+}
+
+function destroyBoxes() {
+  divBoxes.innerHTML = "";
+}
