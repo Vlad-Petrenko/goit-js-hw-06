846f6e4af3cf6a182277d2640cccca1473f0e416
Vlad-Petrenko <vlad120595@gmail.com>
2022-03-28 17:29:50 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -11,7 +11,7 @@ btnCreate.addEventListener("click", createBoxes);
 btnDestroy.addEventListener("click", destroyBoxes);
 
 function createBoxes(amount) {
-  amount = Number(inputNumber.value);
+  amount = inputNumber.value;
   const array = [];
 
   for (let i = 1; i <= amount; i += 1) {
