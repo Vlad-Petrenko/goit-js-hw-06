9a5a0a26da7fa51b190dc48a9e636a3646f9beee
Vlad-Petrenko <vlad120595@gmail.com>
2022-03-28 20:01:28 +0300

    внес правки

--- js/task-10.js
+++ js/task-10.js
@@ -7,24 +7,27 @@ const btnCreate = document.querySelector("[data-create]");
 const btnDestroy = document.querySelector("[data-destroy]");
 const divBoxes = document.querySelector("div#boxes");
 
-btnCreate.addEventListener("click", createBoxes);
+
+btnCreate.addEventListener("click", getAmount);
 btnDestroy.addEventListener("click", destroyBoxes);
 
+function getAmount(amount) {
+  amount = inputNumber.value;
+  createBoxes(amount);
+};
+
 function createBoxes(amount) {
-  amount = Number(inputNumber.value);
   const array = [];
 
   for (let i = 1; i <= amount; i += 1) {
     array.push(
-      `<div style="background-color: ${getRandomHexColor()}; width: ${
-        20 + i * 10
-      }px; height: ${20 + i * 10}px"></div>`
+      `<div style="background-color: ${getRandomHexColor()}; width: ${20 + i * 10}px; height: ${20 + i * 10}px"></div>`
     );
   }
   divBoxes.insertAdjacentHTML("afterbegin", array.join(""));
+  inputNumber.value = "";
 }
 
 function destroyBoxes() {
-  divBoxes.innerHTML = "";
-  inputNumber.value = "";
+  divBoxes.innerHTML = "";  
 }
